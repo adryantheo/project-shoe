fae587d5940de0dc48393d0eef577460ce0e88c7
Adryan Theo <adryantheo@gmail.com>
2019-01-08 20:20:53 +0700

    Model Product, Users, Orders, Passport

--- app/User.php
+++ app/User.php
@@ -2,13 +2,14 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -27,4 +28,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function orders()
+        {
+            return $this->hasMany(Order::class);
+        }
 }
